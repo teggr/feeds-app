69ae2de93c9f42270ec129b55a142c2e057d2b31
Robin Tegg <robin@tegg.me.uk>
2021-02-27 14:06:58 +0000

    #1 manage podcast

--- src/main/java/com/robintegg/feedsapp/web/PodcastsController.java
+++ src/main/java/com/robintegg/feedsapp/web/PodcastsController.java
@@ -4,6 +4,9 @@ import com.robintegg.feedsapp.podcasts.Podcast;
 import com.robintegg.feedsapp.podcasts.PodcastRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,8 +25,15 @@ public class PodcastsController {
         return "redirect:/";
     }
 
+    @GetMapping("/podcasts/{id}")
+    public String post(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("podcast", podcastRepository.findById(id)
+                .orElseThrow());
+        return "podcasts/podcast";
+    }
+
     @PostMapping("/podcasts/{id}/delete")
-    public String post(@RequestParam("id") Long id) {
+    public String post(@PathVariable("id") Long id) {
         podcastRepository.deleteById(id);
         return "redirect:/";
     }
