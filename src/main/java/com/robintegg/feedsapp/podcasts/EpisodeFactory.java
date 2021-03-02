3a183e07c7d273d182b6ba3d7d71b0bdfb5a2d1d
rtegg <rtegg@trunarrative.com>
2021-03-02 13:51:11 +0000

    fix missing entry data

--- src/main/java/com/robintegg/feedsapp/podcasts/EpisodeFactory.java
+++ src/main/java/com/robintegg/feedsapp/podcasts/EpisodeFactory.java
@@ -5,13 +5,17 @@ import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.ByteArrayInputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 class EpisodeFactory {
 
     @SneakyThrows
@@ -20,46 +24,59 @@ class EpisodeFactory {
         SyndFeedInput input = new SyndFeedInput();
         SyndFeed feed = input.build(new XmlReader(new ByteArrayInputStream(feedData.getBytes())));
 
-        // System.out.println(feed);
-
-        System.out.println(feed.getUri());
-        System.out.println(feed.getTitle());
-        System.out.println(feed.getLink());
-
-        System.out.println(feed.getPublishedDate());
-
-        //   System.out.println("Episodes since " + modifiedSince);
+        log.info("uri={},title={},link={}", feed.getUri(), feed.getTitle(), feed.getLink());
 
         List<Episode> episodes = new ArrayList<>();
 
         List<SyndEntry> entries = feed.getEntries();
         for (SyndEntry entry : entries) {
 
-            // System.out.println(entry.getTitle());
-
-            System.out.println(entry);
+            log.info("uri={},title={},link={}", entry.getUri(), entry.getTitle(), entry.getLink());
 
             if (entry.getPublishedDate() != null) {
 
-                //   Instant pd = entry.getPublishedDate().toInstant();
-                // Instant mod = modifiedSince.toInstant(ZoneOffset.UTC);
-                // System.out.println(pd.getEpochSecond() + " : " + mod.getEpochSecond());
-                // if (pd.isAfter(mod)) {
+                Audio audio = getAudio(entry);
+                URL linkUrl = getLinkUrl(entry);
+                ZonedDateTime publishedDate = getPublishedDate(entry);
+                String title = entry.getTitle();
+
                 episodes.add(
                         new Episode(
-                                entry.getTitle(),
-                                new URL(entry.getLink()),
-                                entry.getPublishedDate().toInstant().atZone(ZoneId.systemDefault()),
-                                new Audio(new URL(entry.getEnclosures().get(0).getUrl()), entry.getEnclosures().get(0).getType())
+                                title,
+                                linkUrl,
+                                publishedDate,
+                                audio
                         ));
-                // }
+
             }
 
         }
 
-        // System.out.println(episodes.size());
-
         return episodes;
 
     }
+
+    private static ZonedDateTime getPublishedDate(SyndEntry entry) {
+        return entry.getPublishedDate().toInstant().atZone(ZoneId.systemDefault());
+    }
+
+    private static URL getLinkUrl(SyndEntry entry) throws MalformedURLException {
+        URL linkUrl = null;
+        if (entry.getLink() != null) {
+            linkUrl = new URL(entry.getLink());
+        }
+        return linkUrl;
+    }
+
+    private static Audio getAudio(SyndEntry entry) throws MalformedURLException {
+        Audio audio;
+        if (entry.getEnclosures().isEmpty()) {
+            log.info("NO ENCLOSURES");
+            audio = new Audio(null, null);
+        } else {
+            audio = new Audio(new URL(entry.getEnclosures().get(0).getUrl()), entry.getEnclosures().get(0).getType());
+        }
+        return audio;
+    }
+
 }
