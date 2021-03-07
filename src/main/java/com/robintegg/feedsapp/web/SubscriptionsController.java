ace5e759e1dbccf8c0e55d7fe825acd28ae3d9fd
Robin Tegg <robin@tegg.me.uk>
2021-03-07 22:50:08 +0000

    close #19 delete subscriptions

--- src/main/java/com/robintegg/feedsapp/web/SubscriptionsController.java
+++ src/main/java/com/robintegg/feedsapp/web/SubscriptionsController.java
@@ -2,14 +2,12 @@ package com.robintegg.feedsapp.web;
 
 import com.robintegg.feedsapp.subscriptions.PodcastSubscriptions;
 import com.robintegg.feedsapp.subscriptions.Subscription;
+import com.robintegg.feedsapp.subscriptions.SubscriptionRepository;
 import com.robintegg.feedsapp.subscriptions.SubscriptionSetup;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,8 +15,9 @@ public class SubscriptionsController {
 
     private final SubscriptionSetup subscriptionSetup;
     private final PodcastSubscriptions podcastSubscriptions;
+    private final SubscriptionRepository subscriptionRepository;
 
-    @PostMapping(path = "/subscriptions")
+    @PostMapping(path = "/subscriptions/subscribe")
     public String postSubscribe(@RequestHeader("Referer") String referer, @RequestParam("podcastId") Long podcastId) {
         Subscription subscription = subscriptionSetup.subscribeToPodcast(podcastId);
         return "redirect:" + referer;
@@ -28,6 +27,12 @@ public class SubscriptionsController {
     public String getPodcastSubscriptions(Model model) {
         model.addAttribute("podcastSubscriptions", podcastSubscriptions.findAll());
         return "subscriptions/subscriptions";
+    }
+
+    @PostMapping(path = "/subscriptions/{id}/unsubscribe")
+    public String postSubscribe(@PathVariable("id") Long subscriptionId) {
+        subscriptionRepository.deleteById(subscriptionId);
+        return "redirect:/subscriptions";
     }
 
 }
