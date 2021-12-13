56b7896566db9974b2afd863572d631e8b3df7ff
teggr <robin@tegg.me.uk>
2021-12-13 09:12:21 +0000

    and security starter

---
+++ src/main/java/com/robintegg/feedsapp/web/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.robintegg.feedsapp.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	@Override
+	protected UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("rtegg").password("password").roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+}
