b78f9bd0ea39f478eb6a0a9de4f7cc22e1b915e6
teggr <robin@tegg.me.uk>
2022-01-05 13:01:53 +0000

    Add remember me feature

--- src/main/java/com/robintegg/feedsapp/web/WebSecurityConfiguration.java
+++ src/main/java/com/robintegg/feedsapp/web/WebSecurityConfiguration.java
@@ -1,11 +1,23 @@
 package com.robintegg.feedsapp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.rememberMe().userDetailsService(this.userDetailsService);
+	}
 
 }
